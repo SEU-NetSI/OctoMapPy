a863f4f4453231d6d24a80ffef459e6a96b47f66
Raven <raven.xu98@gmail.com>
2022-03-20 15:14:25 +0800

    #fix: 更新octonode的注释样式

--- octomap/OctoNode.py
+++ octomap/OctoNode.py
@@ -2,29 +2,32 @@ import math
 
 
 class OctoNode:
-    def __init__(self, prior_prob=0.5):
+    def __init__(self, default_probability: float=0.5):
         """
-        Initiates a new OctoNode
-        prior_prob: the prior probability of this node being occupied
+        Initiates a new OctoNode.
+
+        Args:
+            default_probability: the default probability of this node being occupied, 
+            the probability of non-leaf will be set as 0.
         """
         self._children = None
-        self._log_odds = math.log(prior_prob/1-prior_prob)
+        self._log_odds = math.log(default_probability / (1 - default_probability))
 
     def is_leaf(self):
         """
-        Returns whether this is a leaf node
+        Returns:
+            whether this is a leaf node --- bool
         """
         return self._children is None
 
     def _split(self):
         """
-        Splits the node into 8 child nodes
+        Splits the node into 8 child nodes.
+        Child nodes are given the occupancy probability of this parent node as the initial probability
         """
         if not self.is_leaf():
             return
-        """
-        Child nodes are given the occupancy of this parent node as the initial probability of occupancy
-        """
+        
         self._children = (
             OctoNode(self.probability), OctoNode(self.probability), OctoNode(self.probability),
             OctoNode(self.probability), OctoNode(self.probability), OctoNode(self.probability),
@@ -33,28 +36,43 @@ class OctoNode:
 
     def index(self, point, origin, width):
         """
-        Calculates the index of the child containing point
+        Calculates the index of the child containing point.
+
+        Args:
+            point: the coornidate of the child node --- (x,y,z): tuple
+            origin: the origin coornidate of the parent node -- (x,y,z): tuple
+            width: the width of the parent node --- int
+
+        Returns:
+            the index of the child --- int
         """
         if not self.contains(point, origin, width):
-            raise ValueError('Point is not contained in node')
+            raise ValueError('Point is not contained in node.')
 
-        return (1 if point[0] >= origin[0] + width / 2 else 0) +\
-               (2 if point[1] >= origin[1] + width / 2 else 0) +\
+        return (1 if point[0] >= origin[0] + width / 2 else 0) + \
+               (2 if point[1] >= origin[1] + width / 2 else 0) + \
                (4 if point[2] >= origin[2] + width / 2 else 0)
 
     def contains(self, point, origin, width):
         """
-        Returns whether the point is contained by this node
+        Returns:
+            whether the point is contained by this node --- bool
         """
         return origin[0] <= point[0] < origin[0] + width and \
                origin[1] <= point[1] < origin[1] + width and \
                origin[2] <= point[2] < origin[2] + width
 
-    def origin(self, index, origin, width):
+    def origin(self, index: int, origin: tuple, width: int):
         """
-        Calculates the origin of the node with given index
+        Calculates the origin of the node with given index.
+
+        Args:
+            index: the index of the child node --- int
+            origin: the origin coordinate of the parent node --- (x,y,z): tuple
+            width: the width of the parent node --- int
         """
-        hwidth = width / 2
+        hwidth: int = width / 2
+
         return (origin[0] + (hwidth if index & 1 else 0),
                 origin[1] + (hwidth if index & 2 else 0),
                 origin[2] + (hwidth if index & 4 else 0))
@@ -63,12 +81,12 @@ class OctoNode:
         """
         Updates the node with a new observation.
 
-            Args:
-                point: the point of the observation
-                probability: probability of occupancy
-                origin: origin of this node
-                width: width of this node
-                max_depth: maximum depth this node can be branched
+        Args:
+            point: the point coornidate of the observation --- (x,y,z): tuple
+            probability: probability of occupancy --- float
+            origin: origin of this node --- (x,y,z): tuple
+            width: width of this node --- int
+            max_depth: maximum depth this node can be branched --- int
         """
         if max_depth == 0:
             self._update_probability(probability)
@@ -82,38 +100,34 @@ class OctoNode:
 
     def _update_probability(self, probability):
         """
-        Updates the probability of occupancy
+        Updates the occupancy probability of the leaf node.
+
+        Args:
+            probability: pre-defined probability --- float
         """
         self._log_odds += math.log(probability / (1 - probability))
 
     @property
     def probability(self):
         """
-        Returns probability of occupancy
+        Returns:
+            occupancy probability of node --- float
         """
         odds = math.pow(math.e, self._log_odds)
         return odds / (odds + 1)
 
     def probability_at(self, point, origin, width):
         """
-        Returns probability of occupancy at a given point.
         Args:
-            point: point at which the occupancy needs to be calculated
-            origin: origin of this node
-            width: width of this node
+            point: point at which the occupancy needs to be calculated --- (x,y,z): tuple
+            origin: origin of this node --- (x,y,z): tuple
+            width: width of this node --- int
+
+        Returns:
+            occupancy probability of a given point.
         """
         if self.is_leaf():
             return self.probability
         else:
             child_index = self.index(point, origin, width)
             return self._children[child_index].probability_at(point, self.origin(child_index, origin, width), width / 2)
-
-
-
-
-
-
-
-
-
-
--- octomap/OctoTree.py
+++ octomap/OctoTree.py
@@ -5,7 +5,7 @@ class OctoTree:
     """
     OctoMap to store 3D probabilistic occupancy information.
     """
-    
+
     def __init__(self, center: tuple, resolution: int, max_depth: int):
         """
         Create a new OctoMap.
@@ -70,6 +70,18 @@ class OctoTree:
 
         self._root.update(point, probability, self.origin, self.width, self._max_depth)
 
+    def ray_casting(self, point: tuple, probability: float=1.0):
+        """
+        Add an observation to the treemap.
+
+        Args:
+            point: the coordinate of the ovservation lidar point --- (x,y,z): tuple
+        """
+        if not len(point) == 3:
+            raise ValueError("Point shoule be tuple (x,y,z)")
+        if not 0 < probability < 1:
+            raise ValueError("Probability should be between 0.0 and 1.0")        
+
     def contains(self, point: tuple):
         """
         Return whether the point is contained in this tree.
@@ -99,7 +111,7 @@ class OctoTree:
         if not self.contains(point):
             raise ValueError("Invalid point.")
         
-        probability: float = self.root.probability_at(point, self.origin, self.width)
+        probability: float = self._root.probability_at(point, self.origin, self.width)
 
         return probability
         
