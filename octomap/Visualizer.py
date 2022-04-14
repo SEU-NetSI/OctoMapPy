74e62d5490bccc88ea6d30d1344d01b094faf376
Raven <raven.xu98@gmail.com>
2022-04-14 16:46:39 +0800

    #fix: 画图过慢的问题

--- octomap/Visualizer.py
+++ octomap/Visualizer.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-import time
 
-from Config import OCCUPANY_LOGODDS, FREE_LOGODDS, TREE_MAX_DEPTH, TREE_RESOLUTION, Offset_x, Offset_y, Offset_z
+from Config import TREE_MAX_DEPTH, Offset_x, Offset_y, Offset_z
 from OctoNode import OctoNode
 
 
 class Visualizer:
-    fig = plt.figure(figsize=(15,15))
     def __init__(self) -> None:
-        pass
+        self.fig = plt.figure(figsize=(7,7))
 
     def visualize(self):
         """
@@ -20,14 +18,14 @@ class Visualizer:
         """
         occu_node_coor_list, free_node_coor_list = self.import_known_node()
         self.show(occu_node_coor_list, free_node_coor_list, self.fig)
-        print("occu_node_coor_list:", occu_node_coor_list)
-        # print("free_node_coor_list", free_node_coor_list)
+        print("length - occu_node_coor_list: ", len(occu_node_coor_list))
+        print("length - free_node_coor_list: ", len(free_node_coor_list))
 
     def import_known_node(self):
         occu_node_coor_list = []
         free_node_coor_list = []
         # TODO: read csv
-        filename="\\point_list.xls"
+        filename="/point_list.xls"
         occu_nodes=pd.read_excel(os.path.dirname(os.getcwd()) + filename, sheet_name="occu_node_coor_list",
                                  usecols=(0, 1, 2), skiprows=0)
         occu_node_coor_list = list(map(tuple,occu_nodes.values))
@@ -37,13 +35,12 @@ class Visualizer:
         return occu_node_coor_list, free_node_coor_list
 
 
-    @staticmethod
-    def show(occu_node_coor_list, free_node_coor_list, fig):
+    def show(self, occu_node_coor_list, free_node_coor_list, fig):
         """
         Draw a 3D occupancy grid 
         """
         plt.clf()
-        ax = fig.add_subplot(projection='3d')
+        ax = self.fig.add_subplot(projection='3d')
         indice_length = int(math.pow(2, TREE_MAX_DEPTH))    
         # x,y,z determined by the number of grids in that direction
         x, y, z = np.indices((indice_length, indice_length, indice_length))
@@ -58,33 +55,40 @@ class Visualizer:
 
         """
         Two ways to draw:
-        voxel:Better display but very slow
-        Scatter:The speed is fast but the observation effect is not ideal
+        voxel: Better display but very slow
+        scatter: The speed is fast but the observation effect is not ideal
         """
-
+        # free space
+        voxel_container = None
         for i in range(len(free_node_coor_list)):
             free_voxel = (x >= free_node_coor_list[i][0] + Offset_x) & (x < free_node_coor_list[i][0] + 1 + Offset_x) \
                         & (y >= free_node_coor_list[i][1] + Offset_y) & (y < free_node_coor_list[i][1] + 1 + Offset_y) \
                         & (z >= free_node_coor_list[i][2] + Offset_z) & (z < free_node_coor_list[i][2] + 1 + Offset_z)
-            colors = np.empty(free_voxel.shape, dtype=object)
-            colors[free_voxel] = 'green'
-            ax.voxels(free_voxel, facecolors=colors, edgecolor='k')
+            if voxel_container is not None:
+                voxel_container = np.logical_or(voxel_container, free_voxel)
+            else:
+                voxel_container = free_voxel
+                
+        colors = np.empty(voxel_container.shape, dtype=object)
+        colors[voxel_container] = 'green'
+        ax.voxels(voxel_container, facecolors=colors, edgecolor='k')
 
-            # ax.scatter3D(free_node_coor_list[i][0], free_node_coor_list[i][1], free_node_coor_list[i][2], marker = 's',
-            #           c='g', s =70)
-
+        # occupied space
+        voxel_container = None
         for i in range(len(occu_node_coor_list)):
             occu_voxel = (x >= occu_node_coor_list[i][0] + Offset_x) & (x < occu_node_coor_list[i][0] + 1 + Offset_x) \
                          & (y >= occu_node_coor_list[i][1] + Offset_y) & (y < occu_node_coor_list[i][1] + 1 + Offset_y) \
                          & (z >= occu_node_coor_list[i][2] + Offset_z) & (z < occu_node_coor_list[i][2] + 1 + Offset_z)
-            colors = np.empty(occu_voxel.shape, dtype=object)
-            colors[occu_voxel] = 'red'
-            ax.voxels(occu_voxel, facecolors=colors, edgecolor='k')
+            if voxel_container is not None:
+                voxel_container = np.logical_or(voxel_container, occu_voxel)
+            else:
+                voxel_container = occu_voxel
 
-            # ax.scatter3D(occu_node_coor_list[i][0], occu_node_coor_list[i][1], occu_node_coor_list[i][2], marker='s',
-            #              c='r', s=70)
+        colors = np.empty(voxel_container.shape, dtype=object)
+        colors[voxel_container] = 'red'
+        ax.voxels(voxel_container, facecolors=colors, edgecolor='k')
 
-        # plt.show()
+
         # plt.savefig('./map.jpg', dpi=1200)
 
 
@@ -92,18 +96,16 @@ def main():
     visualizer = Visualizer()
     loop_counter = 0
     plt.ion()
-    # TODO: while loop
-    # while True:
-    #     visualizer.visualize()
-    #     time.sleep(5)
+    
     while loop_counter < 5:
         visualizer.visualize()
         loop_counter += 1
-        print("已插入一组数据")
-        time.sleep(3)
-        plt.pause(0.2)
+        print("Refresh " + str(loop_counter) + " times...")
+        plt.pause(0.1)
+        
     plt.ioff()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
