03407993affb935845e6ce577a0c60cf079b3083
renhaitao123 <1984754817@qq.com>
2022-04-19 16:15:10 +0800

    飞行

---
+++ octomap/Test_Flying.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2017 Bitcraze AB
+#
+#  Crazyflie Nano Quadcopter Client
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+"""
+This script shows the basic use of the MotionCommander class.
+
+Simple example that connects to the crazyflie at `URI` and runs a
+sequence. This script requires some kind of location system, it has been
+tested with (and designed for) the flow deck.
+
+The MotionCommander uses velocity setpoints.
+
+Change the URI variable to your Crazyflie configuration.
+"""
+from codecs import lookup_error
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+        # We take off when the commander is created
+        with MotionCommander(scf,0.1) as mc:
+            # mc.circle_right(0.2, velocity=0.1, angle_degrees=360)
+
+            height=30   # Obstacle height  cm
+            max_counter = height/10 
+            loop_counter=0
+            while loop_counter < max_counter:
+                time.sleep(1)
+                # m m/s
+                mc.right(0.5,velocity=0.1)
+                # degree
+                mc.turn_left(90)
+                mc.right(0.5,velocity=0.1)
+                
+                mc.turn_left(90)
+                mc.right(0.5,velocity=0.1)
+                
+                mc.turn_left(90)
+                mc.right(0.5,velocity=0.1)
+                mc.turn_left(90)
+                
+                mc.up(0.1)
+                loop_counter+=1
+            
+            print("Done")
+            mc.stop()
+
+            
