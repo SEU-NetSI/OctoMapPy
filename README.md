67f9901398559084ec3a7885ca8e1193c7e35771
Raven <raven.xu98@gmail.com>
2022-04-21 12:31:40 +0800

    #docs: 更新配置文件注释

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ OctoMap is an efficient probabilistic 3D mapping framework based on Octrees. Thi
 Hornung, Armin, et al. "OctoMap: An efficient probabilistic 3D mapping framework based on octrees." Autonomous robots 34.3 (2013): 189-206.
 ```
 
-In this repository, we provide a basic implementation of OctoMap, a visualization tool and a path plan algorithm (RRT) working with OctoMap.
+In this repository, we provide a basic implementation of OctoMap and a visualization tool and a path plan algorithm (RRT) working with OctoMap.
 
 # Config
-TREE_CENTER：Indicates an area that can be represented by an octree centered on this point. When the insertion point exceeds this area, the error "Point is not contained in node." will be prompted
+- TREE_CENTER: Indicates an area that can be represented by an octree centered on this point. When the insertion point exceeds this area, the error "Point is not contained in node." will be prompted
 
-SENSOR_TH：The distance measurement of the VL53L1X series laser sensor used by the Multi-ranger deck in this project is accurate within 400cm. This value is determined by the configuration of the selected sensor
+- SENSOR_TH：The distance measurement of the VL53L1X series laser sensor used by the Multi-ranger deck in this project is accurate within 400cm. This value is determined by the configuration of the selected sensor
 
 WIDTH： The width of the experimental scene, determined by the area supported by the light-house in this project. When the insertion point is larger than this range, the point is shrunk to the boundary of the experimental scene
 
--- octomap/Config.py
+++ octomap/Config.py
@@ -1,42 +1,57 @@
 import logging
-import math
 
-# STEP
+"""
+The probability change for each new observation.
+The default value is from the '5.1 Evaluation - Sensor model for laser range data' of the OctoMap Paper.
+"""
 HIT_LOGODDS=0.85
 HIT_PROBABILITY=0.7    
 MISS_LOGODDS=-0.4
-MISS_PROBABILITY=0.4   # The probability value of each update when the occupied/free state is observed
+MISS_PROBABILITY=0.4
 
-# THRESHOLD
+"""
+The probability threshold for occupany and free.
+If there is not threshold, the status change will be very hard and not in time. 
+When the probability of the voxel is arrived at the threshold, it indicates the voxel has been set occupied/free status.
+"""
 OCCUPANY_LOGODDS=3.5
 OCCUPANY_PROBABILITY=0.97
 FREE_LOGODDS=-2
-FREE_PROBABILITY=0.12  # When the node probability is greater than or less than the threshold, 
-                       # it indicates the occupied/free state
- 
-# DEFAULT
+FREE_PROBABILITY=0.12
+
+"""
+Initial probability value.
+"""
 DEFAULT_PROBABILITY=0.5
-DEFAULT_LOGODDS=0       # initial probability value
+DEFAULT_LOGODDS=0
 
-# OCTOTREE
-TREE_RESOLUTION=4       # Spatial resolution:the smaller the value, the higher the spatial resolution
-TREE_MAX_DEPTH=5        # When the resolution is determined, the larger the value, the larger the space that can be represented
-
+"""
+Shape of the OctoTree, a cube with the same width, length and the height.
+TREE_RESOLUTION (cm): the size of each voxel.
+TREE_MAX_DEPTH: the recursion of the tree.
+So, the size of the OctoTree is TREE_RESOLUTION * 2^TREE_MAX_DEPTH.
+"""
+TREE_RESOLUTION=4
+TREE_MAX_DEPTH=5
 # TREE_CENTER=(math.pow(2 , TREE_MAX_DEPTH) * TREE_RESOLUTION / 2,
 #             math.pow(2 , TREE_MAX_DEPTH) * TREE_RESOLUTION / 2,
 #             math.pow(2 , TREE_MAX_DEPTH) * TREE_RESOLUTION / 2)
 # TODO: The setting of the midpoint changes the output
 TREE_CENTER=(50, 50 , 50)     # regional center point
 
-# Crazyflie
-URI='radio://0/80/2M/E7E7E7E7E7'  # URI to the Crazyflie to connect to
-SENSOR_TH=400  # Sensor ranging threshold
+"""
+Crazyflie and its laser sensor.
+"""
+URI='radio://0/80/2M/E7E7E7E7E7'
+SENSOR_TH=400
 PLOT_SENSOR_DOWN=False
 
+"""
+Visualization.
+The coornidate under the OctoTree should be adjusted to the Matplotlib.
+"""
 # SPACE
 WIDTH=256     # The width of the experimental scene
-
-# Visualization
 # Visualize point offsets
 # Offset half the width of the experimental area so that all coordinate points are turned to positive values
 # Offset_x = WIDTH / (2 *TREE_RESOLUTION) 
