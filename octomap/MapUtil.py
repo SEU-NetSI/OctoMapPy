6af4b11ca704bc974431dad670b5d30410ee7aaa
renhaitao123 <1984754817@qq.com>
2022-05-05 11:35:28 +0200

    Experimental drawing data

--- octomap/MapUtil.py
+++ octomap/MapUtil.py
@@ -93,12 +93,7 @@ def get_log_config():
     lmap.add_variable('stabilizer.roll')
     lmap.add_variable('stabilizer.pitch')
     lmap.add_variable('stabilizer.yaw')
-    lmap.add_variable('motor.m1')
-    lmap.add_variable('motor.m2')
-    lmap.add_variable('motor.m3')
-    lmap.add_variable('motor.m4')
-
-
+    
     return lmap
 
 def parse_log_data(data):
