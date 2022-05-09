4ced1b538b7b85f66b38af0b36ff02bb4d4b4b38
Raven <raven.xu98@gmail.com>
2022-05-09 14:45:41 +0800

    #feat: add flyingTest

--- octomap/FlyingTest.py
+++ octomap/FlyingTest.py
@@ -5,20 +5,12 @@ from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.position_hl_commander import PositionHlCommander
 
-from Config import URI, LOGGER, TREE_CENTER, TREE_MAX_DEPTH, TREE_RESOLUTION, WHETHER_FLY, OBSTACLE_HEIGHT, TAKEOFF_HEIGHT, SIDE_LENGTH,FLIGHT_SPEED
-from OctoTree import OctoTree
-from PathPlan import PathPlan
-from MapUtil import get_log_config, parse_log_data, get_end_point
+from Config import URI, LOGGER, WHETHER_FLY, OBSTACLE_HEIGHT, TAKEOFF_HEIGHT, SIDE_LENGTH,FLIGHT_SPEED
 
-
-class OctoMap:
+class FlyingTest:
     def __init__(self):
-        self.octotree = OctoTree(TREE_CENTER, TREE_RESOLUTION, TREE_MAX_DEPTH)
-        self.path_planner = PathPlan()
-        self.counter = 0
-        LOGGER.info("OctoTree has been build, the coordinate range is from {} to {}".
-        format(-TREE_RESOLUTION * math.pow(2, TREE_MAX_DEPTH) / 2, TREE_RESOLUTION * math.pow(2, TREE_MAX_DEPTH) / 2))
-        
+        pass
+
     def start(self):
         cflib.crtp.init_drivers()
         self.cf = Crazyflie(ro_cache=None, rw_cache='cache')
@@ -35,16 +27,6 @@ class OctoMap:
         self.cf.open_link(URI)
         LOGGER.info('We are now connected to {}'.format(URI))
         with SyncCrazyflie(URI, cf=self.cf) as scf:
-            try:
-                lmap = get_log_config()
-                self.cf.log.add_config(lmap)
-                lmap.data_received_cb.add_callback(self.update_map)
-                lmap.start()
-                LOGGER.info("Log has been configured.")
-            except KeyError as e:
-                LOGGER.error('Could not start log configuration,''{} not found in TOC'.format(str(e)))
-            except AttributeError:
-                LOGGER.error('Could not add Measurement log config, bad configuration.')
             if WHETHER_FLY:
                 print("ready to fly")
                 with PositionHlCommander(crazyflie=scf, 
@@ -75,24 +57,9 @@ class OctoMap:
     def disconnected(self, URI):
         LOGGER.info('Disconnected with {}'.format(URI))
 
-    def update_map(self, timestamp, data, logconf):
-        
-        # start_time = time.time()
-        measurement, start_point = parse_log_data(data)
-        end_points = get_end_point(start_point, measurement)
-        for end_point in end_points:
-            self.octotree.ray_casting(tuple(start_point), tuple(end_point))
+def main():
+    flyingTest = FlyingTest()
+    flyingTest.start()
 
-        # export nodes each 100 times ranging
-        self.counter += 1
-        # TODO: new a thread to export
-        if self.counter % 100 == 0:
-            self.octotree.export_known_voxel()
-        
-        # end_time = time.time()
-        # print('Running time: %s s' % ((end_time - start_time)))
-    
-    #TODO: Random Search Path Planning
-    def plan_path(self, start_point=(0, 0, 0), end_point=(10, 10, 10)):
-        path: list = self.path_planner.planning(start_point, end_point)
-        return path
+if __name__=="__main__":
+    main()
