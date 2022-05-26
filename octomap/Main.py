fc29a15e6be3779ec8cc1b82f027c2ee28f68609
Raven <raven.xu98@gmail.com>
2022-05-26 13:04:17 +0800

    #feat: add interactor module

--- octomap/Main.py
+++ octomap/Main.py
@@ -1,10 +1,34 @@
+import os
+from multiprocessing import Process
+from queue import Queue
+
 from OctoMap import OctoMap
 
 
+def info(title):
+    print(title)
+    print('Module name:', __name__)
+    print('Parent process:', os.getppid())
+    print('Process id:', os.getpid())
 
-def main():
+def task_octomap():
+    info("task_octomap")
     octomap = OctoMap()
     octomap.start()
+
+def task_interaction():
+    info("task_interaction")
+    pass
+
+def main():
+    queue = Queue()
+    p_octomap = Process(target=task_octomap, args=queue)
+    p_interaction = Process(target=task_interaction, args=queue)
+
+    p_octomap.start()
+    # p_interaction.start()
+    p_octomap.join()
+    # p_interaction.join()
 
 if __name__=="__main__":
     main()
